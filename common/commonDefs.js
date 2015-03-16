0831d6053ccebf783bd569d0b8b0c636887ffec0
slamzhzm <slamzhzm@gmail.com>
2015-03-16 21:24:07 +0800

    hellodsdasfdasdfasdfacreate commondefs

---
+++ common/commonDefs.js
@@ -0,0 +1,14 @@
+/**
+ * Created by slamzhzm on 2015/3/16.
+ */
+
+var EventNetworkLED = {
+    SC_LOGIN : "SC_LOGIN",
+    SC_GAMEID : "SC_GAMEID",
+    SC_PREPARE_STATE : "SC_PREPARE_STATE",
+    SC_START_GAME : "SC_START_GAME",
+    SC_GAME_STATE : "SC_GAME_STATE",
+    SC_UPDATE_STATE : "SC_UPDATE_STATE",
+    SC_END_GAME : "SC_END_GAME"
+}
+
