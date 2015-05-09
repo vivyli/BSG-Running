652c3fed23e527447c7461fce6d0becfe1cfd6f2
Li Yaochun <vivylics@gmail.com>
2015-05-10 00:15:48 +0800

    add log

--- server/workflow.js
+++ server/workflow.js
@@ -3,6 +3,7 @@
  */
 
 require('../common/commonDefs.js');
+var log = require('log.js');
 
 // Game State -> Opened event
 // WAITING_FOR_PLAYER   ->  Player.Login, Player.HeartBeat
@@ -43,6 +44,7 @@ function check_accept_event(game_state, event) {
             break;
     }
 
+    log.log_with_color('[WORKFLOW-CONTROL] FAILED: state = ' + game_state + ' event = ' + event);
     return false;
 }
 
